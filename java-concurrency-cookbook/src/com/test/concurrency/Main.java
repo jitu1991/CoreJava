bd6156508c61119e280cf41186951fdd276299ad
Jitender Kumar <jitender.kumar@irissoftware.com>
2019-05-05 18:34:56 +0530

    AtomicIntegerArray Implementation

--- java-concurrency-cookbook/src/com/test/concurrency/Main.java
+++ java-concurrency-cookbook/src/com/test/concurrency/Main.java
@@ -3,10 +3,8 @@ package com.test.concurrency;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.Thread.State;
-import java.util.Map;
-import java.util.concurrent.ConcurrentNavigableMap;
-import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
@@ -851,6 +849,39 @@ public class Main {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}*/
+		
+		/************************* Using AtomicIntegerArray **********************/
+		final int THREADS = 100;
+		AtomicIntegerArray vector = new AtomicIntegerArray(1000);
+
+		Incrementer incrementer = new Incrementer(vector);
+		Decrementer decrementer = new Decrementer(vector);
+
+		Thread threadIncrementer[] = new Thread[THREADS];
+		Thread threadDecrementer[] = new Thread[THREADS];
+
+		for (int i = 0; i < THREADS; i++) {
+			threadIncrementer[i] = new Thread(incrementer);
+			threadDecrementer[i] = new Thread(decrementer);
+
+			threadIncrementer[i].start();
+			threadDecrementer[i].start();
+		}
+
+		for (int i = 0; i < 100; i++) {
+			try {
+				threadIncrementer[i].join();
+				threadDecrementer[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		for (int i = 0; i < vector.length(); i++) {
+			if (vector.get(i) != 0) {
+				System.out.println("Vector[" + i + "] : " + vector.get(i));
+			}
+		}
+		System.out.println("Main: End of the example");	
 	}
 	
 
